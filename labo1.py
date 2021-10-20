86fa6a2a07e59db1eef1d7d0696be1704545105b
Bryan Ramirez <bryanramirez1220@gmail.com>
2021-10-20 16:06:29 -0600

    Versión final

--- labo1.py
+++ labo1.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
 
+char = input('Ingrese el caracter de la pirámide ')
+while True:
 
-print('hello world')
+    try:
+        itera = int(input('Ingrese el tamaño de la base de la pirámide '))
+        break
+    except:
+        print('El valor ingresado no es válido, por favor ingrese un número entero ')
+
+for x in range (itera+1):
+
+    
+    print (x*char)
